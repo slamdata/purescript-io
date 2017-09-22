1946aa590bf34999fbe22d06c845650fe80783af
Felix Schlitter <felixschlitter@protonmail.com>
2017-09-23 06:40:30 +1200

    Add MTL instances

--- src/Control/Monad/IO/Class.purs
+++ src/Control/Monad/IO/Class.purs
@@ -1,6 +1,16 @@
 module Control.Monad.IO.Class where
 
+import Data.Monoid (class Monoid)
 import Control.Monad.IO (IO)
+import Control.Monad.Cont.Trans (ContT)
+import Control.Monad.Except.Trans (ExceptT)
+import Control.Monad.List.Trans (ListT)
+import Control.Monad.Maybe.Trans (MaybeT)
+import Control.Monad.Reader.Trans (ReaderT)
+import Control.Monad.RWS.Trans (RWST)
+import Control.Monad.State.Trans (StateT)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Writer.Trans (WriterT)
 import Prelude
 
 class (Monad m) <= MonadIO m where
@@ -8,3 +18,27 @@ class (Monad m) <= MonadIO m where
 
 instance monadIOIO :: MonadIO IO where
   liftIO = id
+
+instance monadIOContT :: MonadIO m => MonadIO (ContT r m) where
+  liftIO = lift <<< liftIO
+
+instance monadIOExceptT :: MonadIO m => MonadIO (ExceptT e m) where
+  liftIO = lift <<< liftIO
+
+instance monadIOListT :: MonadIO m => MonadIO (ListT m) where
+  liftIO = lift <<< liftIO
+
+instance monadIOMaybe :: MonadIO m => MonadIO (MaybeT m) where
+  liftIO = lift <<< liftIO
+
+instance monadIOReader :: MonadIO m => MonadIO (ReaderT r m) where
+  liftIO = lift <<< liftIO
+
+instance monadIORWS :: (MonadIO m, Monoid w) => MonadIO (RWST r w s m) where
+  liftIO = lift <<< liftIO
+
+instance monadIOState :: MonadIO m => MonadIO (StateT s m) where
+  liftIO = lift <<< liftIO
+
+instance monadIOWriter :: (MonadIO m, Monoid w) => MonadIO (WriterT w m) where
+  liftIO = lift <<< liftIO
--- src/Control/Monad/IOSync/Class.purs
+++ src/Control/Monad/IOSync/Class.purs
@@ -1,9 +1,19 @@
 module Control.Monad.IOSync.Class where
 
+import Data.Monoid (class Monoid)
+import Data.Newtype (unwrap, wrap)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.IO (IO)
 import Control.Monad.IOSync (IOSync)
-import Data.Newtype (unwrap, wrap)
+import Control.Monad.Cont.Trans (ContT)
+import Control.Monad.Except.Trans (ExceptT)
+import Control.Monad.List.Trans (ListT)
+import Control.Monad.Maybe.Trans (MaybeT)
+import Control.Monad.Reader.Trans (ReaderT)
+import Control.Monad.RWS.Trans (RWST)
+import Control.Monad.State.Trans (StateT)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Writer.Trans (WriterT)
 import Prelude
 
 class (Monad m) <= MonadIOSync m where
@@ -14,3 +24,27 @@ instance monadIOSyncIOSync :: MonadIOSync IOSync where
 
 instance monadIOSyncIO :: MonadIOSync IO where
   liftIOSync = wrap <<< liftEff <<< unwrap
+
+instance monadIOSyncContT :: MonadIOSync m => MonadIOSync (ContT r m) where
+  liftIOSync = lift <<< liftIOSync
+
+instance monadIOSyncExceptT :: MonadIOSync m => MonadIOSync (ExceptT e m) where
+  liftIOSync = lift <<< liftIOSync
+
+instance monadIOSyncListT :: MonadIOSync m => MonadIOSync (ListT m) where
+  liftIOSync = lift <<< liftIOSync
+
+instance monadIOSyncMaybe :: MonadIOSync m => MonadIOSync (MaybeT m) where
+  liftIOSync = lift <<< liftIOSync
+
+instance monadIOSyncReader :: MonadIOSync m => MonadIOSync (ReaderT r m) where
+  liftIOSync = lift <<< liftIOSync
+
+instance monadIOSyncRWS :: (MonadIOSync m, Monoid w) => MonadIOSync (RWST r w s m) where
+  liftIOSync = lift <<< liftIOSync
+
+instance monadIOSyncState :: MonadIOSync m => MonadIOSync (StateT s m) where
+  liftIOSync = lift <<< liftIOSync
+
+instance monadIOSyncWriter :: (MonadIOSync m, Monoid w) => MonadIOSync (WriterT w m) where
+  liftIOSync = lift <<< liftIOSync
